10cc08643a183f1a301e963be111fea776793b9d
ash-shameikh <43913770+ash-shameikh@users.noreply.github.com>
2024-09-19 06:46:43 +0300

    s

--- index.php
+++ index.php
@@ -3,11 +3,11 @@
 $lang = isset($_GET['lang']) ? $_GET['lang'] : 'en';
 
 // Determine the content file based on the language
-$contentFile = 'content/index-' . $lang . '.html';
+$contentFile = 'home-1-ar' . $lang . '.html';
 
 // Check if the content file exists
 if (!file_exists($contentFile)) {
-    $contentFile = 'content/index-en.html'; // Fallback to English if file not found
+    $contentFile = 'home-1'; // Fallback to English if file not found
 }
 
 // Include the content file
